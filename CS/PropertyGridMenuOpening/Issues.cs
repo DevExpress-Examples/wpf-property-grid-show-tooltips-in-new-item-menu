160d9f33f0ce71b15f4137fe36c485a4d170ae5d
AndreySlabov <andrey.slabov@devexpress.com>
2022-10-24 16:43:27 +0400

    Add Example

---
+++ CS/PropertyGridMenuOpening/Issues.cs
@@ -0,0 +1,42 @@
+﻿using DevExpress.Mvvm.DataAnnotations;
+using System.Collections.Generic;
+
+namespace PropertyGridMenuOpening {
+    public class IssueView {
+        public string ProductName { get; set; }
+        [NewItemInstanceInitializer(typeof(Bug))]
+        [NewItemInstanceInitializer(typeof(Question))]
+        [NewItemInstanceInitializer(typeof(BreakingChange))]
+        public IssueList Issues { get; internal set; }
+    }
+    public class IssueList : List<Issue> {
+        public override string ToString() {
+            return "Issues";
+        }
+    }
+    public abstract class Issue {
+        public string Header { get; set; }
+        public string Owner { get; set; }
+
+    }
+    public enum Severity {
+        Minor,
+        Moderate,
+        Severe
+    }
+    public class Bug : Issue {
+        public Severity Severity { get; set; }
+        public string Assignee { get; set; }
+    }
+    public class Question : Issue {
+        public string Assignee { get; set; }
+    }
+    public enum ChangeType {
+        APIChange,
+        UIChange,
+        BehaviorChange
+    }
+    public class BreakingChange : Issue {
+        public ChangeType ChangeType { get; set; }
+    }
+}
---
+++ CS/PropertyGridMenuOpening/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using DevExpress.Xpf.Core;
+using DevExpress.Xpf.PropertyGrid;
+
+namespace PropertyGridMenuOpening {
+    public partial class MainWindow : ThemedWindow {
+        public MainWindow() {
+            InitializeComponent();
+            propertyGrid.SelectedObject = new IssueView {
+                ProductName = "PropertyGridControl",
+                Issues = new IssueList {
+                    new Bug { Header = "Rendering problem", Assignee = "Jack Plank", Owner = "Adam Smith", Severity = Severity.Severe },
+                    new Question { Header = "Rendering problem", Assignee = "Elsa Lynch", Owner = "Adam Smith" },
+                    new BreakingChange { Header = "Layout update", ChangeType = ChangeType.BehaviorChange, Owner = "Adam Smith" }
+                }
+            };
+        }
+        private void OnMenuOpening(object sender, PropertyGridMenuEventArgs e) {
+            if (e.MenuType == PropertyGridMenuType.NewItem && e.Row.Path == "Issues") {
+                e.Items[0].ToolTip = "Create a new breaking change.";
+                e.Items[1].ToolTip = "Report a new bug.";
+                e.Items[2].ToolTip = "Ask a new question.";
+            }
+        }
+    }
+}
